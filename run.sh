dc026b817d382d2a4cd8b9a5dbf1019c221eacc8
Richard Conway <richard@rconway.co.uk>
2022-06-15 13:27:12 +0100

    Fix-up permission problems for local volume mount

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ ORIG_DIR="$(pwd)"
 cd "$(dirname "$0")"
 BIN_DIR="$(pwd)"
 
+export PUSER="$(id -un)"
+export PUID="$(id -u)"
+export PGID="$(id -g)"
+
 function onExit() {
   docker-compose -f docker-compose-dev.yml down
   cd "${ORIG_DIR}"
