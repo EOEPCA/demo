5844f3a20702fa94325e5385ff88a3f9b19f85a2
Richard Conway <richard.conway@telespazio.com>
2025-03-10 10:52:11 +0000

    Remove not needed kubectl/kubeconfig support (#17)

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ export PGID="$(id -g)"
 
 function onExit() {
   docker-compose -f docker-compose-dev.yml down
-  rm -f kubeconfig
   cd "${ORIG_DIR}"
 }
 trap "onExit" EXIT
@@ -22,13 +21,6 @@ fi
 if ! hash docker-compose 2>/dev/null; then
   echo "ERROR - docker-compose is required" 1>&2
   exit 1
-fi
-
-touch kubeconfig
-if hash kubectl 2>/dev/null; then
-  if kubectl config view --flatten --minify 2>/dev/null >kubeconfig; then
-    chmod 600 kubeconfig
-  fi
 fi
 
 docker-compose -f docker-compose-dev.yml up --build
