7872ddc8b2025d923837a50929fdb3044797cf4c
Richard Conway <richard@rconway.co.uk>
2025-03-10 10:51:40 +0000

    Remove not needed kubectl/kubeconfig support

--- build.sh
+++ build.sh
@@ -8,12 +8,8 @@ trap "cd '${ORIG_DIR}'" EXIT
 
 source ./container-info
 
-# eval $(minikube -p minikube docker-env)
-
 docker-compose build
 docker tag ${REPOSITORY}:latest ${REPOSITORY}:${TAG}
-
-# eval $(minikube -p minikube docker-env -u)
 
 echo -e "\nCreated docker image: ${REPOSITORY}:latest => ${REPOSITORY}:${TAG}\n"
 
